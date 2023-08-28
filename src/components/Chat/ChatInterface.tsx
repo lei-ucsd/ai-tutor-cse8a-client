86c8ab72cd1a391e87b46dd9827eb3a7ea091b37
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2023-08-27 18:35:57 -0700

    basic interactivity for sending and rendering user messages

--- src/components/Chat/ChatInterface.tsx
+++ src/components/Chat/ChatInterface.tsx
@@ -3,41 +3,86 @@ import React from "react";
 import { Paper } from "@mui/material";
 import { TextInput } from "./TextInput";
 import { MessageOther, MessageSelf } from "./Message";
+import { useState } from "react";
+import LoadingSpinner from "../UtilElements/LoadingSpinner";
+
+const initMsgs = [
+    <MessageSelf
+        message="I am a fake human user. I am a fake human user. I am a fake human user. I am a fake human user. I am a fake human user. I am a fake human user. "
+        timestamp="MM/DD 00:00"
+        photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+        displayName="User (Fake)"
+        avatarDisp={false}
+    />,
+    <MessageSelf
+        message="Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. "
+        timestamp="MM/DD 00:00"
+        photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+        displayName="User (Fake)"
+        avatarDisp={true}
+    />,
+    <MessageOther
+        message="I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. "
+        timestamp="MM/DD 00:00"
+        photoURL=""
+        displayName="AI Tutor (Fake)"
+        avatarDisp={false}
+    />,
+    <MessageOther
+        message="Welcome to Chat! Welcome to Chat! Welcome to Chat! Welcome to Chat! Welcome to Chat! Welcome to Chat! "
+        timestamp="MM/DD 00:00"
+        displayName="AI Tutor (Fake)"
+        avatarDisp={true}
+    />
+]
 
 export default function ChatInterface() {
+
+    const [msgs, setMsgs] = useState(initMsgs);
+
+    const [showSpinner, setShowSpinner] = useState(false);
+
+    const spinner = <LoadingSpinner />;
+
+    const addMsg = (msg: string) => {
+        const newMsgs = [
+            <MessageSelf
+                message={msg}
+                timestamp="MM/DD 00:00"
+                photoURL=""
+                displayName="User (Fake)"
+                avatarDisp={false}
+            />,
+            ...msgs
+        ];
+
+        setMsgs(newMsgs);
+
+        setShowSpinner(true);
+
+        setTimeout(() => {
+            setMsgs(
+                [
+                    <MessageOther
+                        message={"AI is offline at the moment."}
+                        timestamp="MM/DD 00:00"
+                        displayName="AI Tutor (Fake)"
+                        avatarDisp={true}
+                    />,
+                    ...newMsgs
+                ]
+            );
+            setShowSpinner(false);
+        }, 5000);
+    }
     return (
         <div className="chatContainer">
             <Paper className="paper">
                 <Paper id="style-1" className="messagesBody">
-                    <MessageOther
-                        message="Welcome to Chat! Welcome to Chat! Welcome to Chat! Welcome to Chat! Welcome to Chat! Welcome to Chat! "
-                        timestamp="MM/DD 00:00"
-                        displayName="AI Tutor (Fake)"
-                        avatarDisp={true}
-                    />
-                    <MessageOther
-                        message="I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. I am a fake AI tutor. "
-                        timestamp="MM/DD 00:00"
-                        photoURL=""
-                        displayName="AI Tutor (Fake)"
-                        avatarDisp={false}
-                    />
-                    <MessageSelf
-                        message="Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. Hello, fake AI tutor. "
-                        timestamp="MM/DD 00:00"
-                        photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
-                        displayName="User (Fake)"
-                        avatarDisp={true}
-                    />
-                    <MessageSelf
-                        message="I am a fake human user. I am a fake human user. I am a fake human user. I am a fake human user. I am a fake human user. I am a fake human user. "
-                        timestamp="MM/DD 00:00"
-                        photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
-                        displayName="User (Fake)"
-                        avatarDisp={false}
-                    />
+                    {showSpinner ? spinner : <></>}
+                    {msgs}
                 </Paper>
-                <TextInput />
+                <TextInput onAddMsg={addMsg} />
             </Paper>
         </div>
     )
--- src/components/Chat/TextInput.tsx
+++ src/components/Chat/TextInput.tsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import SendIcon from '@mui/icons-material/Send';
 import { TextField, Button } from '@mui/material'
+import { useState } from 'react';
 
 
-export function TextInput() {
+export function TextInput({ onAddMsg }) {
+    const [value, setValue] = useState("");
+
     return (
         <>
-            <form className="wrapForm"  noValidate autoComplete="off">
-            <TextField
-                id="standard-text"
-                label="Message"
-                className="wrapText"
+            {/* <div className="wrapForm" noValidate autoComplete="off"> */}
+            <div className="wrapForm">
+                <TextField
+                    id="standard-text"
+                    label="Message"
+                    className="wrapText"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
                 // margin="normal"
-            />
-            <Button variant="contained" color="primary" className="button">
-                <SendIcon />
-            </Button>
-            </form>
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className="button"
+                    onClick={
+                        () => {
+                            onAddMsg(value)
+                            setValue("")
+                        }
+                    }
+                >
+                    <SendIcon />
+                </Button>
+            </div>
         </>
     )
 }
