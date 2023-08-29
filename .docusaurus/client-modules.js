19120152553513360affb3e0fce22fa74f64f1cc
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2023-08-28 17:46:21 -0700

    remove unrelated files

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/lisa/projects/ai-tutor-cse8a/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/lisa/projects/ai-tutor-cse8a/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/lisa/projects/ai-tutor-cse8a/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/lisa/projects/ai-tutor-cse8a/src/css/custom.css'),
-  require('/Users/lisa/projects/ai-tutor-cse8a/node_modules/@rise4fun/docusaurus-plugin-rise4fun/lib/rise4fun.css'),
-];
