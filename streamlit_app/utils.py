1d768e4f8f94fb8638842b428348d0ee366c2ab9
Prudhviraj Naidu <prnaidu@ucsd.edu>
2023-09-02 23:50:03 -0700

    Removing Langchain #2

--- streamlit_app/utils.py
+++ streamlit_app/utils.py
@@ -1,5 +1,5 @@
 import random
-from langchain.memory import ConversationBufferMemory
+# from langchain.memory import ConversationBufferMemory
 
 from typing import List, Dict, Any
 
@@ -12,16 +12,16 @@ def get_random_idx(num_questions):
 	for idx in indices:
 		yield idx
 
-class ExtendedConversationBufferMemory(ConversationBufferMemory):
-    extra_variables:List[str] = []
+# class ExtendedConversationBufferMemory(ConversationBufferMemory):
+#     extra_variables:List[str] = []
 
-    @property
-    def memory_variables(self) -> List[str]:
-        """Will always return list of memory variables."""
-        return [self.memory_key] + self.extra_variables
+#     @property
+#     def memory_variables(self) -> List[str]:
+#         """Will always return list of memory variables."""
+#         return [self.memory_key] + self.extra_variables
 
-    def load_memory_variables(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
-        """Return buffer with history and extra variables"""
-        d = super().load_memory_variables(inputs)
-        d.update({k:inputs.get(k) for k in self.extra_variables})        
-        return d
+#     def load_memory_variables(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+#         """Return buffer with history and extra variables"""
+#         d = super().load_memory_variables(inputs)
+#         d.update({k:inputs.get(k) for k in self.extra_variables})        
+#         return d
